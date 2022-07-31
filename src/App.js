93ec82c0f448324aa7d7ca12367e276940b7b15f
jstraye1 <jstraye1@gmail.com>
2022-07-31 16:40:17 -0500

    added github link

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ export default function App() {
     <div className="App container">
       <div className="card weather-app-container">
         <div className="card-body">
-          <h1>Hi</h1>
           <Weather />
         </div>
       </div>
+      <p className="coded-by">
+        <a
+          href="https://github.com/jstraye1/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-Source Code
+        </a>{" "}
+        by Jen Strayer
+      </p>
     </div>
   );
 }
